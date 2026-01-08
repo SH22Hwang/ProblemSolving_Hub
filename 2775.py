a67605f5a07356bd36ce4029eea67690533e75dd
홍길동 <ghkd3531@naver.com>
2026-01-08 09:58:46 +0900

    부녀회장은 DP로 해결

--- 2775.py
+++ 2775.py
@@ -3,7 +3,7 @@
 import sys
 input = sys.stdin.readline
 
-import pprint
+# import pprint
 
 def main():
     t = int(input())
@@ -16,26 +16,18 @@ def main():
         countApt(k, n)
         print(apt[k][n])
 
-    # aptt = [[1] + list(range(1, 15))]
-    # for i in range(1, 15): # i: 층수, k호실까지
-    #     aptt.append([0] * 15)
-    #     if not aptt[i][0]:
-    #         for j in range(1, 15): 
-    #             aptt[i][j] += aptt[i-1][j] + aptt[i][j-1]
-            
-    #     aptt[i][0] = 1    
-
     # pprint.pprint(apt)
+
+
 def countApt(k, n):
-    apt.append([0] * 15)
     for f in range(1, k+1): # 1부터 k층까지
-        if not apt[f][0]: # 이거 왜 넣었더라
-            for r in range(1, n+1): # 1호부터 n호까지
+        apt.append([0] * 15)
+        for r in range(1, n+1): # 1호부터 n호까지
+            if not apt[f][r]:
                 apt[f][r] += apt[f-1][r] + apt[f][r-1]
-            
-        apt[f][0] = 1
+
 
 
 if __name__ == "__main__":
-    apt = [[0] + list(range(1, 15))]
+    apt = [list(range(15))]
     main()
